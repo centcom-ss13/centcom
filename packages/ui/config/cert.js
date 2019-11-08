89a82bc18f2c458816c380f138894099c101bccc
Steven Kneisler <stevenkneisler@gmail.com>
2019-11-08 02:22:58 -0500

    Readd cert config

---
+++ packages/ui/config/cert.js
@@ -0,0 +1,11 @@
+module.exports = {
+  databaseUrl: 'mysql',
+  databaseUsername: 'root',
+  databasePassword: 'wowItsMeAPassword',
+  apiHost: '',
+  apiPort: 3000,
+  frontEndUrl: '134.209.42.179',
+  frontEndPort: 80,
+  apiSSL: false,
+  frontEndSSL: false,
+};
